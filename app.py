6be779252751a1ce53074dfcc23a6920a1599c59
Nixonite <programminglinguist@gmail.com>
2018-04-13 20:42:25 -0700

    added serialization to api models using marshmallow

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api, reqparse
+from flask_httpauth import HTTPBasicAuth
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 import models
 import os
 
@@ -14,7 +17,15 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://{usr}:{dbpass}@{host}:5432
 models.db.init_app(app)
 with app.app_context():
     models.db.create_all()
-api = Api(app)
+api = Api(app, prefix='/api')
+auth = HTTPBasicAuth()
+limiter = Limiter(
+    app,
+    key_func=get_remote_address,
+    default_limits['40 per minute', '1 per second'],
+)
+
+puzzleSerializer = models.PuzzleSerializer()
 
 class Puzzle(Resource):
     parser = reqparse.RequestParser()
@@ -24,7 +35,9 @@ class Puzzle(Resource):
 
     def get(self, puzzle_id):
         queried_data = models.Puzzle.query.get(puzzle_id)
-        return {puzzle_id: queried_data.fen}
+        result = puzzleSerializer.dump(queried_data)
+        return result
+    
     def post(self):
         data = Puzzle.parser.parse_args()
         new_puzzle = models.Puzzle(fen=data['fen'], game_url=data['url'], solution=data['solution'])
@@ -32,7 +45,7 @@ class Puzzle(Resource):
         models.db.session.commit()
         return data, 201
 
-api.add_resource(Puzzle, '/<int:puzzle_id>')
+api.add_resource(Puzzle, '/puzzle' , '/puzzle/<int:puzzle_id>')
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
--- models.py
+++ models.py
@@ -1,4 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
+from marshmallow import Schema, fields
+from passlib.apps import custom_app_context as pwd_context
 
 db = SQLAlchemy()
 
@@ -15,3 +17,20 @@ class Puzzle(Base):
     
     def __repr__(self):
         return "<Puzzle {}>".format(self.fen)
+
+class PuzzleSerializer(Schema):
+    id = fields.Integer()
+    game_url = fields.Str()
+    fen = fields.Str()
+    solution = fields.Str()
+
+class User(Base):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), index=True)
+    password_hash = db.Column(db.String(128))
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+    
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
